d0e202fef1140260a5a61f0a40ad304dcc6de24e
weekband <gyqja15@gmail.com>
2022-05-12 21:25:53 +0900

    Test

--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -21,6 +21,7 @@ public class OrderServiceImpl implements OrderService{
 
     //생성자 호출시점에 딱 1번만 호출되는 것이 보장된다.
     //불번,필수 의존관계에 사용
+    //commit Test
 
 
     public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy discountPolicy) {
